523e9f5a0081d4edef5e1fcd4eeac0883b0a7899
sangggho <sangggho@gmail.com>
2021-12-15 09:40:41 +0900

    clean dir, etc...

--- chopper.py
+++ chopper.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import cv2 as cv
 
 
-def cut(img_path: str, size=256, overlap_factor=1):
+def kkakdugi(img_path: str, size=256, overlap_factor=1):
     img_name = Path(img_path).name
     save_dir = Path('./chopped') / img_name
     save_dir.mkdir(parents=True, exist_ok=True)
@@ -21,15 +21,15 @@ def cut(img_path: str, size=256, overlap_factor=1):
         h_offset = int((h - size) / max_h_count)
         h_chop_count = max_h_count + 1
 
-        for y in range(h_chop_count):
-            y0 = y * h_offset
-            y1 = y0 + size
-            for x in range(w_chop_count):
-                x0 = x * w_offset
-                x1 = x0 + size
-                region = img[y0:y1, x0:x1]
+        for h in range(h_chop_count):
+            h0 = h * h_offset
+            h1 = h0 + size
+            for w in range(w_chop_count):
+                w0 = w * w_offset
+                w1 = w0 + size
+                region = img[h0:h1, w0:w1]
                 try:
-                    cv.imwrite(f'{save_dir}/{y:02d}_{x:02d}_{img_name}', region)
+                    cv.imwrite(f'{save_dir}/{h:02d}_{w:02d}_{img_name}', region)
                 except cv.error:
                     pass
-                print(f':: "{y:02d}_{x:02d}_{img_name}" saved.')
+                print(f':: "{h:02d}_{w:02d}_{img_name}" saved.')
