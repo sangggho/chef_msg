1d724dbd4c4ccf889d1ca20e91ac07a15ed56564
sangggho <sangggho@gmail.com>
2022-04-10 18:24:01 +0900

    formating

--- chef_msg/stock.py
+++ chef_msg/stock.py
@@ -7,7 +7,7 @@ import cv2 as cv
 import numpy as np
 
 
-def _check_kkakdugi_path(img_path: str, save_dir_name: str = 'dish') -> (str, str):
+def _check_kkakdugi_path(img_path: str, save_dir_name: str = 'dish') -> tuple(str, str):
     img_name = Path(img_path).name
     save_path = Path(save_dir_name) / img_name
     save_path.mkdir(parents=True, exist_ok=True)
@@ -34,8 +34,10 @@ def kkakdugi(img_path: str, size: int = 256, overlap: int = 1):
         h, w, ch = img.shape
         print(f'[::INFO:: READ] {img_name} {img.shape}')
 
-        max_w_count, max_h_count = int(w / size) * overlap, int(h / size) * overlap
-        w_offset, h_offset = int((w - size) / max_w_count), int((h - size) / max_h_count)
+        max_w_count = int(w / size) * overlap
+        max_h_count = int(h / size) * overlap
+        w_offset = int((w - size) / max_w_count)
+        h_offset = int((h - size) / max_h_count)
         w_chop_count, h_chop_count = max_w_count + 1, max_h_count + 1
 
         for h in range(h_chop_count):
@@ -46,14 +48,15 @@ def kkakdugi(img_path: str, size: int = 256, overlap: int = 1):
                 w1 = w0 + size
                 region = img[h0:h1, w0:w1]
                 try:
-                    cv.imwrite(f'{save_dir}/{h:02d}_{w:02d}_{img_name}', region)
+                    cv.imwrite(
+                        f'{save_dir}/{h:02d}_{w:02d}_{img_name}', region)
                 except cv.error:
                     pass
                 print(f'[::WORK:: SAVE] "{h:02d}_{w:02d}_{img_name}"')
 
 
 def salad(img_dir_path: str, test_size: float = 0.2, seed: Optional[int] = None,
-          dir_names: (str, str) = ('train', 'valid')):
+          dir_names: tuple(str, str) = ('train', 'valid')):
     """
     Physically split and shuffle the data(copy) in the directory like salad.
     default test size is 20%(0.2), seed is optional, default directory names 'train' and 'valid'.
@@ -66,7 +69,8 @@ def salad(img_dir_path: str, test_size: float = 0.2, seed: Optional[int] = None,
     np.random.shuffle(files)
     test_index = np.round(len(files) * test_size)
     print(f'[::INFO:: {test_size * 100}% FILES : {int(test_index)}]')
-    print(f'[::INFO:: {(1 - test_size) * 100}% FILES : {int(np.round(len(files))) - int(test_index)}]')
+    print(
+        f'[::INFO:: {(1 - test_size) * 100}% FILES : {int(np.round(len(files))) - int(test_index)}]')
 
     for dir_name in dir_names:
         if not os.path.isdir(f'{dir_name}'):
